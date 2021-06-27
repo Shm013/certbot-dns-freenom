ade9997498682f583e5c87a2e3984d73b06a23db
Nikolay <shm013@yandex.ru>
2021-06-27 21:33:25 +0300

    Test 7

--- tests/test_default.py
+++ tests/test_default.py
@@ -24,7 +24,7 @@ def test_perform():
     authenticator = certbot_dns_freenom._FreenomDNSClient(username, password)
 
     # Empty record
-    authenticator.del_txt_record(domain, record_name, record_target, 30)
+    authenticator.del_txt_record(domain, record_name, record_target, 300)
 
     nslookup_output = os.popen("nslookup -type=TXT {}".format(test_record)).read()
     print(nslookup_output)
@@ -32,7 +32,7 @@ def test_perform():
     assert not record_target in nslookup_output
 
     # Add new record
-    authenticator.add_txt_record(domain, record_name, record_target, 30)
+    authenticator.add_txt_record(domain, record_name, record_target, 300)
     time.sleep(30)
 
     nslookup_output = os.popen("nslookup -type=TXT {}".format(test_record)).read()
