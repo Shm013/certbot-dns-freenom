a51ba55fe1af5836a5870267885956272b545352
Nikolay <shm013@yandex.ru>
2021-06-27 18:27:16 +0300

    Lint

--- tests/test_default.py
+++ tests/test_default.py
@@ -5,6 +5,7 @@ import os
 sys.path.append(str(pathlib.Path().absolute()))
 import certbot_dns_freenom.dns_freenom as certbot_dns_freenom
 
+
 def test_perform():
     assert os.environ["FREENOM_USERNAME"]
     assert os.environ["FREENOM_PASSWORD"]
@@ -12,7 +13,7 @@ def test_perform():
 
     username = os.environ["FREENOM_USERNAME"]
     password = os.environ["FREENOM_PASSWORD"]
-    domain   = os.environ["FREENOM_DOMAIN"]
+    domain = os.environ["FREENOM_DOMAIN"]
 
     print(domain)
 
